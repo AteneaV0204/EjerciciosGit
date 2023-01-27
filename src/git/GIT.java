ebab5e2ac273716902fde45c693ff9ab750221d9
unknown <dam1t12@IC12.jcolonia.local>
2023-01-27 19:46:31 +0100

    Primera imstantanea

---
+++ src/git/GIT.java
@@ -0,0 +1,10 @@
+package git;
+
+public class GIT {
+
+	public static void main(String[] args) {
+		System.out.println("Cosa GIT");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dam1t12
+ *
+ */
+module CosaGIT {
+}
